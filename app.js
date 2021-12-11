c56ad5907d13773d5cf2d9d8f7f3a935ba4e4df3
ivanleongwm <ivanleongwm@gmail.com>
2021-12-11 16:15:50 +0800

    glass panel grid done

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-for (let i=0; i < 90; i++) {
+for (let i=0; i < 400; i++) {
     const glassPanelMap = document.querySelector('.container');
     const newGlassPanel = document.createElement('div');
     newGlassPanel.id = i;
     newGlassPanel.classList.add('panel')
-    //newGlassPanel.innerHTML = 'hello'
     glassPanelMap.appendChild(newGlassPanel)
 
 };
