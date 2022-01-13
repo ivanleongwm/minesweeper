5e310f7c856d45755fbcbf198c0e3b1edc0423bf
ivanleongwm <ivanleongwm@gmail.com>
2022-01-13 19:10:06 +0800

    squid

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var spriteSheet = document.getElementById("sprite-image");
 var widthOfSpriteSheet = 896;
 var widthOfEachSprite = 56;
 var position = widthOfEachSprite; //start position for the image
-let numberOfLives = 'TTTTT';
+let numberOfLives = '🦑🦑🦑🦑🦑';
 
 var brokenPanelsSteppedOn = 0;
 document.querySelector('.fixed-div').innerHTML = `Broken Panels Stepped On: ${brokenPanelsSteppedOn}`
-document.querySelector('.fixed-div-right').innerHTML = numberOfLives
+document.querySelector('.fixed-div-right').innerHTML = numberOfLives.slice(0,-2)
 
 function stopAnimation() {
     $(function () {
@@ -111,8 +111,7 @@ async function stepOnPanel(destinationPanel) {
         await sleep(1000)
         destinationPanel.className = 'panelbroken'
         brokenPanelsSteppedOn += 1
-        numberOfLives = numberOfLives.slice(0,-1)
-        document.querySelector('.fixed-div-right').innerHTML = numberOfLives
+        numberOfLives = numberOfLives.slice(0,-2)
         document.querySelector('.fixed-div').innerHTML = `Broken Panels Stepped On: ${brokenPanelsSteppedOn}`
 
     } else {
